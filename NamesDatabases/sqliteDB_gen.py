7210b3f448f2ca6c6c83b6df1c50dd6f29e9aee1
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2021-07-23 20:54:18 -0700

    Added names table

--- NamesDatabases/sqliteDB_gen.py
+++ NamesDatabases/sqliteDB_gen.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+namesTable = '''CREATE TABLE names
+                (name text, lang text)'''
+
 import sqlite3
 
 con = sqlite3.connect('names.db')
+
+con.execute(namesTable)
+
+con.commit()
 
 con.close()
