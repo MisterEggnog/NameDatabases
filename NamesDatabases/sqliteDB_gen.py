2f78d27276edff64c4ef8089a9a98152b8633359
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2021-07-23 21:11:22 -0700

    Remove old database file before creating new one

--- NamesDatabases/sqliteDB_gen.py
+++ NamesDatabases/sqliteDB_gen.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import sqlite3
+import os
 
+database = 'names.db'
 namesTable = '''CREATE TABLE names
                 (name text, lang text)'''
 addFirstName = '''INSERT INTO names (name)
                   VALUES (?)'''
 
-import sqlite3
+try:
+    os.remove(database)
+except:
+    pass
 
-con = sqlite3.connect('names.db')
+con = sqlite3.connect(database)
 
 con.execute(namesTable)
 
